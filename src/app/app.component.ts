2a781fca92828883e0b623a96fc71172157c508b
mrlost <mrlost@yahoo.com>
2023-07-05 13:42:13 +0330

    clipboard2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {
-    this.password = '111';
+    this.password = '';
   }
   title = 'AngularGeneratePassword';
 
